4adea1bd1a19fcebca203e980315b3ab7790b6f5
dk <cllct@ya.ru>
2021-10-24 00:24:11 +0300

    message

---
+++ app1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app1
+//
+//  Created by Dmitriy Karakulov on 23.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // hello world motherfucka
+    }
+
+
+}
+
